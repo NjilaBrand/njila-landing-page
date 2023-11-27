60c68bce62fe953c9772e7f44401091dfb9cdb96
Maurício Daniel Mbala <mauriciombalajr@gmail.com>
2023-11-27 21:40:22 +0100

    Create Grupo Card separator

--- app/components/desktop/GrupoCard/GrupoCard.tsx
+++ app/components/desktop/GrupoCard/GrupoCard.tsx
@@ -1,8 +1,9 @@
+// "use client"
 import './GrupoCard.scss'
 
-export const GrupoCard = () => {
+export const GrupoCard = async() => {
     const cards: number[] = [1, 2, 3, 4, 5, 6]
-
+    
   return (
     <div className='GrupoCard'>
         {cards.map((card) => (
